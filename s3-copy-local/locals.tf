f93e0baa68c9f13c7f2778db3e602f5e47f29874
vjrmalla <67913193+vjrmalla@users.noreply.github.com>
2021-09-08 15:05:36 +0100

    Update locals.tf

--- s3-copy-local/locals.tf
+++ s3-copy-local/locals.tf
@@ -1,1 +1,33 @@
-
+locals {
+  instance_user_data = {
+  "groups": [
+    "infraopsmi"
+  ],
+  "users": [
+    "default",
+    {
+      "name": "opsmicopy",
+      "sudo": [
+        "ALL=(ALL) NOPASSWD:ALL"
+      ],
+      "groups": "infraopsmi",
+      "shell": "/bin/bash"
+    }
+  ],
+  "write_files": [
+    {
+      "owner": "root:root",
+      "path": "/tmp/s3copy.sh",
+      "permissions": "0754",
+      "encoding" = "b64",
+      "content": filebase64("${path.module}/s3copy.sh")
+    },
+    {
+      "owner": "root:root",
+      "path": "/etc/cron.d/s3copy_cron",
+      "permissions": "0644",
+      "content": "*/5 * * * * root /bin/bash /tmp/s3copy.sh"
+    }
+  ]
+}
+}
